24c491f3bf73ffdeedb31a7a1baf045e9a567c10
SHANbic <supagoof924@hotmail.com>
2020-03-08 11:48:56 +0100

    suppression dossier inutile

--- tout savoir sur/sites anexes/Tout savoir sur Minecraft/script/acceuil.js
+++
@@ -1,7 +0,0 @@
-let menu = document.getElementById('menu');
-let ul = document.getElementsByTagName('ul')
-menu.addEventListener('click', function () {
-    if (ul[0].classList.contains('hidden')) ul[0].classList.remove('hidden');
-    else ul[0].classList.add('hidden')
-})
-ul.addEventListener
